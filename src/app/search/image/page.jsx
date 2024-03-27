9a479ae7acd7efd324bc94cb587084b8e6c57489
Adrian Netto <adrian.netto73@gmail.com>
2024-03-27 11:45:47 -0300

    adding loading effect

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -2,8 +2,8 @@ import ImageSearchResults from "@/components/ImageSearchResults";
 import Link from "next/link";
 
 export default async function ImageSearchPage({ searchParams }) {
-  const startIndex = searchParams.start || '1'
-
+  const startIndex = searchParams.start || "1";
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
