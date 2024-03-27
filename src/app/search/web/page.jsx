abf106574bda2ca7ed6ab061d3e231ede07d4106
Adrian Netto <adrian.netto73@gmail.com>
2024-03-27 11:25:21 -0300

    adding pagination

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -2,8 +2,9 @@ import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 
 export default async function WebSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&${startIndex}'}`
   );
 
   if (!response.ok) throw new Error("Something went wrong");
@@ -13,13 +14,13 @@ export default async function WebSearchPage({ searchParams }) {
 
   if (!results) {
     return (
-      <div className='flex flex-col justify-center items-center pt-10'>
-        <h1 className='text-3xl mb-4'>
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
           No results found for {searchParams.searchTerm}
         </h1>
-        <p className='text-lg'>
-          Try searching the web or images for something else{' '}
-          <Link href='/' className='text-blue-500 '>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500 ">
             Home
           </Link>
         </p>
@@ -27,7 +28,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <div>{results && <WebSearchResults results={data}/>}</div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
